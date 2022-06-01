1d4b25702068c45872af436200a4d79254e9e085
Criblle <97399458+Criblle@users.noreply.github.com>
2022-06-01 20:27:04 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,1 +1,15 @@
+# import os
+# import sys 
+
+# print("Запуск!")
+
+# if os.sys.platform == "win32":
+#     os.system("cls")
+# else:
+#     os.system("clear")
+    
+    
+    
+# os.system()
+
 import StarZed
